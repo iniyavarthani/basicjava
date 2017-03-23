e414ca6c5d423e7cab4ac83d084da71d0f32ab8b
iniyavarthani <iniyamullai26@gmail.com>
2017-03-23 19:04:12 +0530

    Add files via upload

---
+++ FactorialUsingFor.java
@@ -0,0 +1,21 @@
+package lesson4;
+
+public class FactorialUsingFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b,c;
+		b=10;
+		c=5;
+		System.out.println("swapping");
+		b=c+b;
+		c=b-c;
+		b=b-c;
+		System.out.println("value of b is"+b+"and c is"+c);
+		
+		
+	}}
+		 
+
+	
+
