050e77802c8ad56235c882d3e6ed33186e353215
iniyavarthani <iniyamullai26@gmail.com>
2017-03-23 18:57:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+for(int n=0; n<5; n++)
+{
+	
+System.out.println("the value of n:"+n);
+	}
+
+}}
